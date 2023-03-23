9cf591fb5f3f1773e96f25335e94424304a8a824
Lucie Philippon <aelerinya@gmail.com>
2023-03-23 23:16:00 +0100

    stage 4

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
+from multiprocessing import Process
 import os
-import subprocess
+from pathlib import Path
+from shutil import copy
 import sys
+from tempfile import TemporaryDirectory
 
 
-root_folder = os.environ.get("MY_DOCKER_ROOT")
+root_folder = Path(os.environ.get("MY_DOCKER_ROOT") or "/").resolve()
+
+
+def start_in_chroot(chroot_directory: Path, command: str, args: list[str]):
+    os.chroot(chroot_directory)
+    os.execv(command, [command, *args])
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-
-    # Uncomment this block to pass the first stage
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    path = root_folder + command if root_folder is not None else command
-
-    completed_process = subprocess.run([path, *args])
-    exit(completed_process.returncode)
+    with TemporaryDirectory() as dir:
+        dir = Path(dir)
+        old_command_path = (root_folder / ("./" + command)).resolve()
+        new_command_path = (dir / ("./" + command)).resolve()
+        new_command_path.parent.mkdir(parents=True)
+        copy(old_command_path, new_command_path)
+        subprocess = Process(target=start_in_chroot, args=[dir, command, args])
+        subprocess.start()
+        subprocess.join()
+    exit(subprocess.exitcode)
 
 
 if __name__ == "__main__":
