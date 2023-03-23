df720bb1bd3815bc1c4ef68a4dab51d5ecca34f8
Lucie Philippon <aelerinya@gmail.com>
2023-03-23 12:20:05 +0100

    first stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+import os
 import subprocess
 import sys
+
+
+root_folder = os.environ.get("MY_DOCKER_ROOT")
 
 
 def main():
@@ -7,12 +11,13 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[3]
-    # args = sys.argv[4:]
-    #
-    # completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(completed_process.stdout.decode("utf-8"))
+    command = sys.argv[3]
+    args = sys.argv[4:]
+
+    path = root_folder + command if root_folder is not None else command
+
+    completed_process = subprocess.run([path, *args], capture_output=True)
+    print(completed_process.stdout.decode("utf-8"))
 
 
 if __name__ == "__main__":
